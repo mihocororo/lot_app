a6458f40824489867b71e56b9e6363e3c4bbd549
EC2 Default User <ec2-user@ip-172-31-8-175.ap-northeast-1.compute.internal>
2023-05-11 15:36:53 +0000

    top/lot

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,12 +1,30 @@
-let title = "javascriptが使えました";
-alert(title);
-$(document).ready(function () {
-  $('.jquery').on('click', function(){
-    $(this).css('color','red');
-  });
-});
+$(function() {
+ function omikuji() {
+  const cont = Math.ceil(Math.random()*3);
+  if(cont == 1){
+    $(".omikuji_name").html("大吉が出ました");
+    $(".omikuji_text").html("大吉が出ました！おめでとうございます！テキストテキストテキストテキストテキストテキストテキストテキスト");
+    $(".omikuji_img").attr("src","https://f.uploader.xzy.pw/eu-prd/upload/20191108160744_61514c415a.jpg");
+}
+    if(cont == 2){
+    $(".omikuji_name").html("中吉が出ました");
+    $(".omikuji_text").html("中吉が出ました！やったね！テキストテキストテキストテキストテキストテキストテキストテキスト");
+    $(".omikuji_img").attr("src","https://f.uploader.xzy.pw/eu-prd/upload/20191108160418_5165755153.jpg");
+}
+    if(cont == 3){
+    $(".omikuji_name").html("小吉が出ました");
+    $(".omikuji_text").html("小吉が出ました！もうひとふんばり！テキストテキストテキストテキストテキストテキストテキストテキスト");
+    $(".omikuji_img").attr("src","https://f.uploader.xzy.pw/eu-prd/upload/20191108160447_6f6d664c4f.jpg");
+}
+ }
 
-// Swiperのオプションを定数化
+
+    $(".omiguji_btn").on("click",function () {
+        omikuji();
+    });
+    });
+
+    // Swiperのオプションを定数化
 const opt = {
   loop: true,
   pagination: {
