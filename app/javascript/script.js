1f85f58bec65a7d31e36bf3aabe8095b17b8fc8a
EC2 Default User <ec2-user@ip-172-31-8-175.ap-northeast-1.compute.internal>
2023-05-15 14:55:24 +0000

    daikyou

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,29 +1,11 @@
 $(function() {
- function omikuji() {
-  const cont = Math.ceil(Math.random()*3);
-  if(cont == 1){
-    $(".omikuji_name").html("大吉が出ました");
-    $(".omikuji_text").html("大吉が出ました！おめでとうございます！テキストテキストテキストテキストテキストテキストテキストテキスト");
-    $(".omikuji_img").attr("src","https://f.uploader.xzy.pw/eu-prd/upload/20191108160744_61514c415a.jpg");
-}
-    if(cont == 2){
-    $(".omikuji_name").html("中吉が出ました");
-    $(".omikuji_text").html("中吉が出ました！やったね！テキストテキストテキストテキストテキストテキストテキストテキスト");
-    $(".omikuji_img").attr("src","https://f.uploader.xzy.pw/eu-prd/upload/20191108160418_5165755153.jpg");
-}
-    if(cont == 3){
-    $(".omikuji_name").html("小吉が出ました");
-    $(".omikuji_text").html("小吉が出ました！もうひとふんばり！テキストテキストテキストテキストテキストテキストテキストテキスト");
-    $(".omikuji_img").attr("src","https://f.uploader.xzy.pw/eu-prd/upload/20191108160447_6f6d664c4f.jpg");
-}
- }
 
-
-    $(".omiguji_btn").on("click",function () {
-        omikuji();
-    });
-    });
-
+  // 画像をクリックしたら発動
+  $('div.kuji_btn').on('click', function() {
+var random = Math.floor(Math.random() * $('.kuji').length);
+$('.kuji').hide().eq(random).show();
+});
+});
     // Swiperのオプションを定数化
 const opt = {
   loop: true,
@@ -40,3 +22,4 @@ const opt = {
 $(document).on('turbolinks:load', function() {
     let swiper = new Swiper('.swiper',opt);
 });
+
