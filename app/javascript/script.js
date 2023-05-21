7e6b563f911f90e0cb24e8bfee32a6fdbc1c43a4
EC2 Default User <ec2-user@ip-172-31-8-175.ap-northeast-1.compute.internal>
2023-05-21 14:55:13 +0000

    roll

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -18,3 +18,22 @@ $(document).on('turbolinks:load', function() {
     let swiper = new Swiper('.swiper',opt);
 });
 
+$(function() {
+  $('#back a').on('click',function(event){
+    $('body, html').animate({
+      scrollTop:0
+    }, 800);
+    event.preventDefault();
+  });
+});
+
+
+// $(function() {
+//     $('#roll').on('click',function(event){
+//       $('').animate({
+
+//       })
+//     })
+// })
+
+
