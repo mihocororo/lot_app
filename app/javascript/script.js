9fb357d19dc46ce63f4ee09c4cdb9375ad3e5079
EC2 Default User <ec2-user@ip-172-31-8-175.ap-northeast-1.compute.internal>
2023-06-11 14:49:37 +0000

    roulette/

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -154,3 +154,48 @@ $(function(){
     // $('.'+$(this).attr('id')).toggle();
   });
 });
+
+
+
+// ルーレット
+  $(document).on('turbolinks:load', function(){
+      var roulette     = $("#roulette");
+      var start_button = $("#start");
+      var stop_button  = $("#stop");
+      var reset_button = $("#reset");
+
+  //初期状態ではリセットボタンを非表示、ストップボタンをクリック不可にしておく
+      reset_button.css("display", "none");
+      stop_button.prop("disabled", true);
+
+  // スタートボタンを押した時の処理
+   $(start_button).on("click", function () {
+      //スタートボタンをクリック不可、ストップボタンをクリック可能にする
+    $(this).prop("disabled", true);
+      stop_button.prop("disabled", false);
+    //ルーレット盤に .rotate を付与
+      roulette.addClass("rotate");
+    });
+
+  // ストップボタンを押した時の処理
+    $(stop_button).on("click",function () {
+    //ルーレット盤に .stop を付与
+      roulette.addClass("stop");
+    //リセットボタンを表示、スタートボタンを非表示にする
+      reset_button.css("display", "inline-block");
+      start_button.css("display", "none");
+    });
+
+  // リセットボタンを押した時の処理
+    $(reset_button).on("click",function () {
+    //ルーレット盤から .rotate と .stop を取り除く
+      roulette.removeClass("rotate");
+      roulette.removeClass("stop");
+    //リセットボタンを非表示、スタートボタンを表示
+      reset_button.css("display", "none");
+      start_button.css("display", "inline-block");
+    //スタートボタンをクリック可、ストップボタンをクリック不可にする
+      start_button.prop("disabled", false);
+      stop_button.prop("disabled", true);
+    });
+});
