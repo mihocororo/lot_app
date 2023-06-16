a0985775d22982ce6aefc3d391ef28d5b12f6189
EC2 Default User <ec2-user@ip-172-31-8-175.ap-northeast-1.compute.internal>
2023-06-16 14:18:22 +0000

    shuffle

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'gatya'=>'fortunes#gatya'
   get 'cat' =>'homes#cat'
   get 'color' =>'homes#color'
+  get 'shuffle' => 'fortunes#shuffle'
   # get 'homes/top'
   root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
